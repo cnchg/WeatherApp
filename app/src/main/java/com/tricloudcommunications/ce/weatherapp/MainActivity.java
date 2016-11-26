c6ac0115381e31583fe9e3882a9050f2dfbb572e
“Ori <chandleretienne@gmail.com>
2016-11-26 06:14:45 -0600

    Update

--- app/src/main/java/com/tricloudcommunications/ce/weatherapp/MainActivity.java
+++ app/src/main/java/com/tricloudcommunications/ce/weatherapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -27,23 +29,33 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    RelativeLayout contentMainBackground;
     ImageView nearMeImageView;
     ImageView localConditionIconImageView;
     ImageView localForecastHighIconImageView;
@@ -77,6 +89,14 @@ public class MainActivity extends AppCompatActivity {
     String localLocationCountry = "";
     String localLocationSunRise = "";
     String localLocationSunSunset = "";
+
+    //Flickr Variables
+    String flickrPhotoID = "";
+    String flickrPhotoOwnerID = "";
+    String flickrPhotoFarmID = "";
+    String flickrPhotoServerID = "";
+    String flickrPhotoSecret = "";
+
 
     public void getWeather(View view) {
 
@@ -163,6 +183,48 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void setFlickrGlobalVariables(
+            String flickrPhotoID,
+            String flickrPhotoOwnerID,
+            String flickrPhotoFarmID,
+            String flickrPhotoServerID,
+            String flickrPhotoSecret
+    ){
+
+        //Log.i("Photo ID", photoID);
+
+        //String values of all the data we want from the JSON response
+        //flickrPhotoID = photoID;
+        //flickrPhotoOwnerID = photOwnerID;
+
+        //FlickrImageDownloader flickrImageDownloader = new FlickrImageDownloader();
+        //flickrImageDownloader.execute("http://www.flickr.com/photos/"+photoOwnerID+"/"+photoID);
+
+        //start and execute the ImageDownloader() class to download the weather icon image
+        FlickrImageDownloader downloadImageTask = new FlickrImageDownloader();
+        Bitmap myImage;
+
+        try {
+
+            //myImage = downloadImageTask.execute("http://openweathermap.org/img/w/"+iconImageName+".png").get();
+            //myImage = downloadImageTask.execute("http://openweathermap.org/img/w/10d.png").get();
+            //myImage = downloadImageTask.execute("https://www.flickr.com/photos/"+photoOwnerID+"/"+photoID).get();
+            myImage = downloadImageTask.execute("https://farm"+flickrPhotoFarmID+".staticflickr.com/"+flickrPhotoServerID+"/"+flickrPhotoID+"_"+flickrPhotoSecret+".jpg").get();
+
+            //Source: http://stackoverflow.com/questions/7466900/how-to-use-relativelayout-setbackgrounddrawable-with-a-bitmap
+            Drawable drawable = new BitmapDrawable(getResources(), myImage);
+            contentMainBackground.setBackground(drawable);
+
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -233,11 +295,24 @@ public class MainActivity extends AppCompatActivity {
         }
         // Register the listener with the Location Manager to receive location updates
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
+        if (lastKnownLocation != null){
 
+            // Called when a new location is found by the network location provider.
+            double longitude = lastKnownLocation.getLongitude();
+            double latitude = lastKnownLocation.getLatitude();
+            localLongitude = String.valueOf(longitude);
+            localLatitude = String.valueOf(latitude);
+            //nearMeImageView.setVisibility(View.VISIBLE);
+            //locationManager.removeUpdates(locationListener);
 
+            Log.i("Last Known Location", lastKnownLocation.toString());
+
+        }
 
         //Set up the global variables needed for this app
+        contentMainBackground = (RelativeLayout) findViewById(R.id.content_main);
         nearMeImageView = (ImageView) findViewById(R.id.nearMeImageView);
         localConditionIconImageView = (ImageView) findViewById(R.id.localConditionIconImageView);
         localForecastHighIconImageView = (ImageView) findViewById(R.id.localForeCastHighIconImageView);
@@ -298,6 +373,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
 
     }
 
@@ -394,15 +470,36 @@ public class MainActivity extends AppCompatActivity {
                 localLocationSunRise = sysInfo.getString("sunrise");
                 localLocationSunSunset = sysInfo.getString("sunset");
 
-                //code for localLocationSunRise
+                //code for converting localLocationSunRise from Unix time to data format
                 long sunRise = Long.valueOf(localLocationSunRise)*1000;// its need to be in milisecond
                 Date encodedSunrise = new java.util.Date(sunRise);
                 String finalSunRise = new SimpleDateFormat("MM dd, yyyy hh:mma").format(encodedSunrise);
 
-                //code for localLocationSunSet
+                //code for  converting localLocationSunSet from Unix time to data format
                 long sunSet = Long.valueOf(localLocationSunSunset)*1000;// its need to be in milisecond
                 Date encodedSunset = new java.util.Date(sunSet);
                 String finalSunSet = new SimpleDateFormat("MM dd, yyyy hh:mma").format(encodedSunset);
+
+
+                //Initialize the FlickrAPIDataDownload() class which downloads the API JSON data and trigger a call to the setFlickrGlobalVariables() method with then down load the image from Flickr and set it as the background image.
+                try {
+                    String flickrTextTag = URLEncoder.encode(localConditionDescription,"UTF-8");// Properly format the string to URL format
+                    String flickrTagsTag = URLEncoder.encode(localName, "UTF-8");
+
+                    /*
+                     Source1: https://www.flickr.com/services/api/explore/flickr.photos.search
+                     Source2: https://www.flickr.com/services/api/misc.urls.html
+                     Note: To get back a clean JSON response (no extra text in the front or back)-
+                     Make sure that you 'Do not sign call' by adding this to the end of the url string '&format=json&nojsoncallback=1'
+                    */
+                    FlickrAPIDataDownload getFlickrDataTask = new FlickrAPIDataDownload();
+                    //getFlickrDataTask.execute("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=65dd98b8db61ede0ad592cb221e31201&tags=sun&media=photos&per_page=1&page=1&format=json");
+                    getFlickrDataTask.execute("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=65dd98b8db61ede0ad592cb221e31201&text="+flickrTextTag+"&tags="+flickrTagsTag+"&media=photos&per_page=1&page=1&format=json&nojsoncallback=1");
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
 
                 //Log.i("Local Temp", mainInfo.getString("temp"));
                 //Log.i("Local Name", localName);
@@ -456,40 +553,62 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*
-    public class DownLoadTask extends AsyncTask<String, Void, String>{
-
+    public class FlickrAPIDataDownload extends AsyncTask<String, Void, String>{
 
         @Override
-        protected String doInBackground(String... urls) {
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //relativeLayout.setBackgroundColor(Color.DKGRAY);
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            /*
+               Note: the code below from the source below is for making a HTTPS connection and then reading the response
+               from that secure connection. In this case it's for Flickr but can be used in any other case where a secure
+               Https connection needs to be establish before communicating with a host.
+               Source1: https://www.codota.com/android/scenarios/52fcbd97da0ab8e225ec74c6/javax.net.ssl.HttpsURLConnection?tag=dragonfly
+               Source2: http://stackoverflow.com/questions/16504527/how-to-do-an-https-post-from-android
+            */
 
             String result = "";
-            URL url;
-            HttpURLConnection urlConnection;
 
-            try {
-                url = new URL(urls[0]);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = urlConnection.getInputStream();
-                InputStreamReader reader = new InputStreamReader(inputStream);
+            try{
+                //Set up Https request
+                URL url = new URL(params[0]);
+                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                String input = params[0];
+                Log.i("Flickr API Content","Sending: "+input);
+                OutputStream os = conn.getOutputStream();
+                os.write(input.getBytes());
+                os.flush();
+                os.close();
+
+                //Read the data from the response
+                InputStream is = conn.getInputStream();
+                InputStreamReader reader = new InputStreamReader(is);
                 int data = reader.read();
-
-                while (data != -1){
+                while (data !=-1){
                     char current = (char) data;
                     result += current;
-
                     data = reader.read();
                 }
 
+                //Log.i("Flickr API Content","API call sent with result "+result);
                 return result;
 
-            } catch (Exception e) {
-
-                e.printStackTrace();
-
+            } catch (IOException e) {
+                //Log.e("Flickr API Content","Error sending API call", e);
             }
 
             return null;
+
         }
 
         @Override
@@ -497,38 +616,100 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(result);
 
 
-            try{
+
+            try {
 
                 JSONObject jsonObject = new JSONObject(result);
+                JSONObject photoObject = jsonObject.getJSONObject("photos");
+                JSONArray photoArray = photoObject.getJSONArray("photo");
 
-                JSONObject mainInfo = jsonObject.getJSONObject("main");
-                Log.i("Main Temp", mainInfo.getString("temp"));
+                /*
+                NOTE:
+                  We have only one photo in this exercise, if we wanted or had more photos
+                  the we would use the array key number to get the photo with that key
+
+                  Take a look at the chosen answer from this source:
+                  http://stackoverflow.com/questions/19205527/json-parsing-in-android-flickr-api
+                  Example of JSON for more than one Image in the photo array
+                    JSONObject json = new JSONObject(jsonString);
+                    JSONObject photos = json.getJSONObject("photos");
+                    JSONArray photo = photos.getJSONArray("photo");
+                    if (photo.length() > 0) {
+                        JSONObject first = photo.getJSONObject(0);
+                        String picOwner = first.getString("owner");
+                        String picID = first.getString("id");
+                    }
+                */
+
+                JSONObject flickerPhotos = photoArray.getJSONObject(0);
+
+                flickrPhotoID = flickerPhotos.getString("id");
+                flickrPhotoOwnerID = flickerPhotos.getString("owner");
+                flickrPhotoFarmID = flickerPhotos.getString("farm");
+                flickrPhotoServerID = flickerPhotos.getString("server");
+                flickrPhotoSecret = flickerPhotos.getString("secret");
 
 
-                String weatherInfo = jsonObject.getString("weather");
-                JSONArray jsonArrWeatherInfo = new JSONArray(weatherInfo);
-                for (int i = 0; i < jsonArrWeatherInfo.length(); i++){
+                setFlickrGlobalVariables(flickrPhotoID,flickrPhotoOwnerID,flickrPhotoFarmID,flickrPhotoServerID,flickrPhotoSecret);
 
-                    JSONObject jsonPartWeatherInfo = jsonArrWeatherInfo.getJSONObject(i);
+                Log.i("Photo Object ID", flickrPhotoID);
+                Log.i("Photo Object Owner", flickrPhotoOwnerID);
+                Log.i("Photo Object farm", flickrPhotoFarmID);
+                Log.i("Photo Object server", flickrPhotoServerID);
+                Log.i("Photo Object secret", flickrPhotoSecret);
 
-                    Log.i("main", jsonPartWeatherInfo.getString("main"));
-                    Log.i("description", jsonPartWeatherInfo.getString("description"));
 
-                    //currentTemp.setText("Temp: " + jsonPartWeatherInfo.getString("main"));
-                    Log.i("API Content", result);
-
-                }
-
-            }catch (Exception e){
+            } catch (JSONException e) {
 
                 e.printStackTrace();
-                Toast.makeText(getApplicationContext(),"Something Went Wrong",Toast.LENGTH_LONG).show();
+
             }
 
+            //Log.i("On Post Execute", result);
         }
     }
-    */
 
+    public class FlickrImageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            String result;
+            URL url;
+            HttpURLConnection urlConnection;
+
+            try {
+
+                //Set up Https request
+                url = new URL(urls[0]);
+                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                String input = urls[0];
+                Log.i("Flickr API Content","Sending: "+input);
+                OutputStream os = conn.getOutputStream();
+                os.write(input.getBytes());
+                os.flush();
+                os.close();
+
+                InputStream inputStream = conn.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
